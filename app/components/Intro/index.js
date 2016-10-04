409da9047054756ec10a77f98454dacad36a47d0
Emmanuel Isaac <deejecks@live.com>
2016-10-04 12:19:45 +0100

    Homepage WIP

--- app/components/Intro/index.js
+++ app/components/Intro/index.js
@@ -16,16 +16,19 @@ function Intro() {
       <div className={`container ${styles.introMessage}`}>
         <h1 className="display-1">Welcome to packem.com</h1>
         <p className="lead">Find the right gift for the right occasion.</p>
-        <p className="m-b-0">Select from our numerous hampers or Create your own.</p>
-        <p>Quality just got more attractive.</p>
         <p>
-          <Button
-            bsSize="large"
-            className={styles.packemButton}
-          >
-            Start Shopping Now
-          </Button>
+          Select from our numerous hampers or Create your own.
+          Quality just got more attractive.
         </p>
+      </div>
+
+      <div className={styles.startShoppingBtn}>
+        <Button
+          bsSize="large"
+          className={styles.packemButton}
+        >
+          Start Shopping Now
+        </Button>
       </div>
     </section>
   );
